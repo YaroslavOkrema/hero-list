d54bd8d93ece2409ea63481ac692c31d9662538c
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-11-10 18:32:31 +0200

    feat: add animation

--- src/components/HeroList/HeroList.tsx
+++ src/components/HeroList/HeroList.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import HeroItem from "../HeroItem/HeroItem";
 import {HeroListProps} from "../../interfaces";
 import './HeroList.css'
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 const HeroList: React.FC<HeroListProps> = ({heroes, remove}) => {
     return (
         <div>
             <h1 className="hero__title">Hero List</h1>
-            {heroes.map((hero, index) => (
-                <HeroItem remove={remove} hero={hero} key={hero.id} number={index + 1}/>
-            ))}
+            <TransitionGroup>
+                {heroes.map((hero, index) => (
+                    <CSSTransition
+                        key={hero.id}
+                        timeout={500}
+                        classNames="hero"
+                    >
+                    <HeroItem remove={remove} hero={hero} key={hero.id} number={index + 1}/>
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </div>
     );
 };
