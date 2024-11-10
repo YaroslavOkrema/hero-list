f3d3b70d203cdb56757aae5a357b77d9f254c07a
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-11-11 01:46:57 +0200

    feat: add styles

--- src/components/HeroInfo/HeroInfo.tsx
+++ src/components/HeroInfo/HeroInfo.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {HeroInfoProps} from "../../interfaces";
 import {useHeroInfo} from "./useHeroInfo";
+import './HeroInfo.css';
 
 
 const HeroInfo: React.FC<HeroInfoProps> = ({heroes}) => {
     const {hero, handleBack} = useHeroInfo(heroes);
 
     return (
-        <div>
+        <div className="hero__info">
             <h1>{hero?.name}</h1>
-            <p>{hero?.description}</p>
+            <p className="hero__description">{hero?.description}</p>
             <button className="hero__btn" onClick={handleBack}>Back</button>
         </div>
     );
